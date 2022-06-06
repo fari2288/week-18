00ada8124479b1f358e15398ae36c78de9f53a25
Fariza Kubanova <farizatkubanova@gmail.com>
2022-06-07 00:05:04 +0100

    week 18

--- comments/index.js
+++ comments/index.js
@@ -1,29 +1,31 @@
+
 const button=document.querySelector('button');
-button.addEventListener('click', sendComment);
+button.addEventListener('click', checkMessage);
 
-document.addEventListener('DOMContentLoaded', function(evnt){
+
+document.addEventListener('DOMContentLoaded', function (evnt){
+    evnt.preventDefault();
     let name=localStorage.getItem('name');
-if (name!=null){
-    document.querySelector('#name').value=name;
+    if(name!=null){
+        document.querySelector('#author').value=name;
+    }
+});
+
+function sendMessage(author, comment) {
+    let result=document.querySelector('#result');
+    const reg= /viagra|XXX/gim;
+    result.innerHTML+=`<div id="img1" style='background-image: url(https://pixelbox.ru/wp-content/uploads/2021/05/ava-vk-animal-91.jpg); background-size: cover;'></div><span>${author}: </span><span>${comment.replace(reg, '***')}</span>`;
+    document.querySelector('#comment').value='';
 }
-})
 
-
-function sendComment() {
-    let author=document.querySelector('#name').value;
+function checkMessage() {
+let author=document.querySelector('#author').value;
 let comment=document.querySelector('#comment').value;
-if (localStorage.getItem('name')==null){
-    localStorage.setItem('name', author)
-};
-if (localStorage.getItem('comment')==null){
-    localStorage.setItem('comment', document.querySelector('#container').innerHTML)
-};
-document.querySelector('#comment').value='';
-saveComment(author, comment);
+comment.value='';
+if (localStorage.getItem('name')==null) {
+    localStorage.setItem('name', author);}
+if (localStorage.getItem('comment')==null) {
+    localStorage.setItem('comment', result.innerHTML);}
+    sendMessage(author, comment);
 }
 
-function saveComment(author, comment) {
-let container=document.querySelector('#container');
-let reg=/viagra|xxx/gmi;
-container.innerHTML+=`<span>${author}</span><br><span>${comment.replace(reg, '***')}</span><br>`
-}
